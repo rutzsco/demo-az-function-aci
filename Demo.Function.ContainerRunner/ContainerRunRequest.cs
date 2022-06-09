9354729b4e3b215c5b3c4726bf26f2c4e420d687
Scott Rutz <scrutz@microsoft.com>
2022-06-09 08:57:10 -0500

    v1

---
+++ Demo.Function.ContainerRunner/ContainerRunRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Function.ContainerRunner
+{
+    internal class ContainerRunRequest
+    {
+        public string ContainerGroupName { get; set; }
+        public string Region { get; set; }
+        public string ContainerImage { get; set; }
+        public string ResourceGroupName { get; set; }
+
+    }
+}
---
+++ Demo.Function.ContainerRunner/ExecuteEndpoint.cs
@@ -0,0 +1,61 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Microsoft.Azure.Management.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Authentication;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Azure.Management.ContainerInstance.Fluent.Models;
+
+namespace Demo.Function.ContainerRunner
+{
+    public static class ExecuteEndpoint
+    {
+        [FunctionName("ExecuteEndpoint")]
+        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req, 
+            ExecutionContext context,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            var config = new ConfigurationBuilder()
+                                .SetBasePath(context.FunctionAppDirectory)
+                                .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+                                .AddEnvironmentVariables()
+                                .Build();
+
+            var creds = new AzureCredentialsFactory().FromServicePrincipal(config["ClientId"], config["ClientSecret"], config["TenantId"], AzureEnvironment.AzureGlobalCloud);
+            var azure = Azure.Authenticate(creds).WithSubscription(config["SubscriptionId"]);
+
+            var requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            var request = JsonConvert.DeserializeObject<ContainerRunRequest>(requestBody);
+
+
+            // Create the container group
+            var containerGroup = azure.ContainerGroups.Define(request.ContainerGroupName)
+                .WithRegion(request.Region)
+                .WithExistingResourceGroup(request.ResourceGroupName)
+                .WithLinux()
+                .WithPublicImageRegistryOnly()
+                .WithoutVolume()
+                .DefineContainerInstance(request.ContainerGroupName + "-1")
+                    .WithImage(request.ContainerImage)
+                    .WithExternalTcpPort(80)
+                    .WithCpuCoreCount(1.0)
+                    .WithMemorySizeInGB(1)
+                    .Attach()
+                .WithDnsPrefix(request.ContainerGroupName)
+                .WithRestartPolicy(ContainerGroupRestartPolicy.Never)
+                .Create();
+
+
+            return new OkObjectResult("OK");
+        }
+    }
+}
